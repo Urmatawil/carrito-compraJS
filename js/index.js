f001e686306ae7ad10c1b2bc0384155ddbe9864d
urmatawil <susurrus.ater@gmail.com>
2020-11-11 18:25:04 -0300

    Actualizada tarjetas de producto

--- js/index.js
+++ js/index.js
@@ -22,24 +22,50 @@ async function cargarProductos() {
   productos.forEach((product) => {
     html += `
     <div
-      class="col-span-4 md:col-span-2 lg:col-span-1 xl:col-span-1 flex flex-col items-center max-w-sm"
+      class="col-span-4 md:col-span-2 xl:col-span-1 flex flex-col items-center max-w-sm"
     >
-      <div class="bg-white rounded-md mt-8 py-4 px-4 hover:shadow-xl w-full">
+      <div class="bg-white rounded-xl mt-8 py-4 px-12 md:px-4  hover:shadow-xl w-full">
+        <div>
         <img
           src="${product.image}"
-          class="w-64 rounded-md object-center mb-4 flex mx-auto"
+          class="w-48 h-48 rounded-full object-cover object-center overflow-hidden mb-4 flex mx-auto shadow-md"
           alt=""
         />
+        </div>
           <span class="font-bold text-gray-800 text-base leading-tight">${product.name}</span>
-          <hr class="mt-4">
-          <div class="flex items-center justify-between">
-            <div class="text-sm text-gray-600 font-light">${product.extraInfo}</div>
-            <div class="text-2xl text-red-600 font-bold mb-2">$ ${product.price}</div>
+          <hr class="mt-4 mb-4">
+
+          <div class="text-xs text-gray-600 font-light">${product.extraInfo}</div>
+          <div class="text-xs text-gray-600 font-light">${product.relleno}</div>
+          <div class="text-xs text-gray-600 font-light">${product.delivery}</div>
+
+          <div class="w-full flex items-center space-x-3 justify-end">
+            
+            <div class="block inline-block text-lg text-red-600 font-bold">$ ${product.price}</div>
+
+            <button class="block inline-block flex mx-auto bg-transparent rounded-full hover:text-blue-600 text-blue-300"><i class="fas fa-plus-circle text-xl"></i></button>
           </div>
-          <button class="p-4 flex mx-auto font-semibold bg-blue-400 rounded-3xl text-white">Agregar al carrito</button>
+
+          
       </div>
     </div>`;
   });
 
   document.querySelector("#productos").innerHTML = html;
+}
+
+function abrirCarrito() {
+  const btnCarrito = document.getElementsByClassName("cart-products")[0];
+
+  btnCarrito.classList.forEach((item) => {
+    if (item === "hidden") {
+      btnCarrito.classList.remove("hidden");
+      btnCarrito.classList.add("block");
+    } else {
+      if (item === "block") {
+        btnCarrito.classList.remove("block");
+        btnCarrito.classList.add("hidden");
+      }
+    }
+  });
 }
